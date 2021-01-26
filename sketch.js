26e176d77b45123891f6f469373e30556835223a
Sylathas <37075162+Sylathas@users.noreply.github.com>
2021-01-26 21:14:45 +0100

    Selectable coordinates

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const triangles = [];
 const circles = [];
 const semiCircles = [];
-var myCanvas, r1 = 0, r2 = 0, r3 = 0;
+var myCanvas, r1 = 0, r2 = 0, r3 = 0, latitude, longitude, altitude, lastlat, lastlon, lastalt, hasinput = false;
 
 function preload() {
   loader('T', 10);
@@ -12,7 +12,7 @@ function preload() {
 }
 
 function setup() {
-  myCanvas = createCanvas(500 ,500);
+  myCanvas = createCanvas(600 ,600);
   myCanvas.parent("logo");
   //Create Grid
   rectMode(CENTER);
@@ -21,35 +21,61 @@ function setup() {
   //Create Logo
   imageMode(CENTER);
   angleMode(DEGREES);
-  image(circles[0], 250, 250, 300, 300);
-  image(triangles[0], 250, 250, 300, 300);
-  image(semiCircles[0], 250, 250, 300, 300);
-  image(logoName, 250, 250, 300, 300);
+  image(circles[0], 300, 300, 600, 600);
+  image(triangles[0], 300, 300, 600, 600);
+  image(semiCircles[0], 300, 300, 600, 600);
+  image(logoName, 300, 300, 600, 600);
 
   //Add Coordinates
   textFont(folio);
   push();
   rotate(-90);
   textSize(13);
-  text('45.4681388', -250-150, 250-160);
+  text('0', -300-150, 300-160);
   textAlign(CENTER);
-  text('9.2147358 ', -250, 250-160);
+  text('0', -300, 300-160);
   textAlign(RIGHT);
-  text('124', -250+150, 250-160);
+  text('0', -300+150, 300-160);
   pop();
 
+  lastlat = 0;
+  lastlon = 0;
+  lastalt = 0;
 
-  //Create Button to randomize
-  randButton = createButton('Random');
-  randButton.size(100, 50);
-  randButton.position(windowWidth/2+25, windowHeight/2+250);
-  randButton.mousePressed(changeLogo);
+  //Create latitude input
+  latitude = createInput();
+  latitude.size(100, 20);
+  latitude.position(windowWidth/2-220, windowHeight/2+150);
+
+  //Create longitude input
+  longitude = createInput();
+  longitude.size(100, 20);
+  longitude.position(windowWidth/2-55, windowHeight/2+150);
+
+  //Create altitude input
+  altitude = createInput();
+  altitude.size(100, 20);
+  altitude.position(windowWidth/2+110, windowHeight/2+150);
+
+  addInputPlaceholder();
+
+  //Create Button to generate
+  genButton = createButton('Generate');
+  genButton.size(110, 50);
+  genButton.position(windowWidth/2-220, windowHeight/2+250);
+  genButton.mousePressed(changeLogo);
 
   //Create Button to save
-  saveButton = createButton('Save');
-  saveButton.size(100, 50);
-  saveButton.position(windowWidth/2-125, windowHeight/2+250);
-  saveButton.mousePressed(saveLogo);
+  randButton = createButton('Save');
+  randButton.size(110, 50);
+  randButton.position(windowWidth/2-55, windowHeight/2+250);
+  randButton.mousePressed(saveLogo);
+
+  //Create Button to randomize
+  saveButton = createButton('Randomize');
+  saveButton.size(110, 50);
+  saveButton.position(windowWidth/2+110, windowHeight/2+250);
+  saveButton.mousePressed(randomizeLogo);
 }
 
 function draw() {
@@ -68,29 +94,84 @@ function loader(name, num){
 }
 
 function changeLogo(){
+  hasinput = true;
   clear();
+  if(latitude.value()){
+    lastlat = latitude.value();
+  }
+  if (longitude.value()){
+    lastlon = longitude.value();
+  }
+  if(altitude.value()){
+    lastalt = altitude.value();
+  }
 
-  r1 = floor(random(10));
-  r2 = floor(random(10));
-  r3 = floor(random(10));
+  r1 = checkFirstNumber(lastlat, r1, true);
+  r2 = checkFirstNumber(lastlon, r2, true);
+  r3 = checkFirstNumber(lastalt, r3, false);
+
 
   //Create Logo
-  image(circles[r1], 250, 250, 300, 300);
-  image(triangles[r2], 250, 250, 300, 300);
-  image(semiCircles[r3], 250, 250, 300, 300);
-  image(logoName, 250, 250, 300, 300);
+  image(circles[r3], 300, 300, 600, 600);
+  image(triangles[r1], 300, 300, 600, 600);
+  image(semiCircles[r2], 300, 300, 600, 600);
+  image(logoName, 300, 300, 600, 600);
 
   //Add Coordinates
   textFont(folio);
   push();
   rotate(-90);
   textSize(13);
-  text('45.4681388', -250-150, 250-160);
+  text(lastlat, -300-150, 300-160);
   textAlign(CENTER);
-  text('9.2147358 ', -250, 250-160);
+  text(lastlon, -300, 300-160);
   textAlign(RIGHT);
-  text('124', -250+150, 250-160);
+  text(lastalt, -300+150, 300-160);
   pop();
+}
+
+function randomizeLogo(){
+  hasinput = true;
+  clear();
+
+  lastlat = random(10).toFixed(3);
+  lastlon = random(10).toFixed(3);
+  lastalt = floor(random(2000));
+  addInputPlaceholder(hasinput);
+
+  r1 = checkFirstNumber(lastlat, r1, true);
+  r2 = checkFirstNumber(lastlon, r2, true);
+  r3 = checkFirstNumber(lastalt, r3, false);
+
+  //Create Logo
+  image(circles[r3], 300, 300, 600, 600);
+  image(triangles[r1], 300, 300, 600, 600);
+  image(semiCircles[r2], 300, 300, 600, 600);
+  image(logoName, 300, 300, 600, 600);
+
+  //Add Coordinates
+  textFont(folio);
+  push();
+  rotate(-90);
+  textSize(13);
+  text(lastlat, -300-150, 300-160);
+  textAlign(CENTER);
+  text(lastlon, -300, 300-160);
+  textAlign(RIGHT);
+  text(lastalt, -300+150, 300-160);
+  pop();
+}
+
+function checkFirstNumber(coordinate, r, n){
+  var cordfirst1 = coordinate;
+  if(n){cordfirst1 = (coordinate - floor(coordinate))*10;}
+  const cordfirst2 = str(cordfirst1).charAt(0);
+  const cordfirst3 = Number(cordfirst2);
+  if(cordfirst3 > -1 || cordfirst3 < 10){
+    return cordfirst3;
+  } else{
+    return r;
+  }
 }
 
 function saveLogo(){
@@ -101,34 +182,78 @@ function windowResized() {
     clear();
     randButton.remove();
     saveButton.remove();
+    genButton.remove();
+    latitude.remove();
+    longitude.remove();
+    altitude.remove();
 
     //Create Logo
-    image(circles[r1], 250, 250, 300, 300);
-    image(triangles[r2], 250, 250, 300, 300);
-    image(semiCircles[r3], 250, 250, 300, 300);
-    image(logoName, 250, 250, 300, 300);
+    image(circles[r1], 300, 300, 600, 600);
+    image(triangles[r2], 300, 300, 600, 600);
+    image(semiCircles[r3], 300, 300, 600, 600);
+    image(logoName, 300, 300, 600, 600);
 
     //Add Coordinates
     textFont(folio);
     push();
     rotate(-90);
     textSize(13);
-    text('45.4681388', -250-150, 250-160);
+    text(lastlat, -300-150, 300-160);
     textAlign(CENTER);
-    text('9.2147358 ', -250, 250-160);
+    text(lastlon, -300, 300-160);
     textAlign(RIGHT);
-    text('124', -250+150, 250-160);
+    text(lastalt, -300+150, 300-160);
     pop();
 
-    //Create Button to randomize
-    randButton = createButton('Random');
-    randButton.size(100, 50);
-    randButton.position(windowWidth/2+25, windowHeight/2+250);
-    randButton.mousePressed(changeLogo);
+    //Create latitude input
+    latitude = createInput();
+    latitude.size(100, 20);
+    latitude.position(windowWidth/2-220, windowHeight/2+150);
+
+    //Create longitude input
+    longitude = createInput();
+    longitude.size(100, 20);
+    longitude.position(windowWidth/2-55, windowHeight/2+150);
+
+    //Create altitude input
+    altitude = createInput();
+    altitude.size(100, 20);
+    altitude.position(windowWidth/2+110, windowHeight/2+150);
+
+    addInputPlaceholder(hasinput);
+
+    //Create Button to generate
+    genButton = createButton('Generate');
+    genButton.size(110, 50);
+    genButton.position(windowWidth/2-220, windowHeight/2+250);
+    genButton.mousePressed(changeLogo);
 
     //Create Button to save
-    saveButton = createButton('Save');
-    saveButton.size(100, 50);
-    saveButton.position(windowWidth/2-125, windowHeight/2+250);
-    saveButton.mousePressed(saveLogo);
+    randButton = createButton('Save');
+    randButton.size(110, 50);
+    randButton.position(windowWidth/2-55, windowHeight/2+250);
+    randButton.mousePressed(saveLogo);
+
+    //Create Button to randomize
+    saveButton = createButton('Randomize');
+    saveButton.size(110, 50);
+    saveButton.position(windowWidth/2+110, windowHeight/2+250);
+    saveButton.mousePressed(randomizeLogo);
+}
+
+function addInputPlaceholder(hasinput){
+  i = 0;
+  inputs = document.getElementsByTagName('input');
+  for (input of inputs){
+    if(hasinput){
+      if(i == 0){input.value = lastlat;}
+      else if(i == 1){input.value = lastlon;}
+      else if(i == 2){input.value = lastalt;}
+    } else if(!hasinput){
+      if(i == 0){input.placeholder = "Latitudine";}
+      else if(i == 1){input.placeholder = "Longitudine";}
+      else if(i == 2){input.placeholder = "Altitudine";}
+    }
+    i++;
+  }
 }

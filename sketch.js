afe8554d04a9371b32cf6b4c5168be8fe4094c10
Sylathas <37075162+Sylathas@users.noreply.github.com>
2021-01-28 20:58:08 +0100

    Selectable Type

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
-const triangles = [];
+const trianglesone = [];
+const trianglestwo = [];
+const trianglesthree = [];
+const trianglesfour = [];
+const trianglesfive = [];
+const trianglessix = [];
+const trianglesseven = [];
+const triangleseight = [];
+const trianglesnine = [];
+const trianglesten = [];
 const circles = [];
 const semiCircles = [];
-var myCanvas, r1 = 0, r2 = 0, r3 = 0, latitude, longitude, altitude, lastlat, lastlon, lastalt, hasinput = false;
+var myCanvas, r1 = 0, r2 = 0, r3 = 0, selector, latitude, longitude, altitude, lastlat, lastlon, lastalt, hasinput = false, lastvalue;
 
 function preload() {
   loader('T', 10);
@@ -11,20 +20,33 @@ function preload() {
   folio = loadFont('assets/FolioStd-Light.otf');
 }
 
+function loader(name, num){
+  for(let i = 0; i < num; i++){
+    if(name === 'T'){
+      trianglesone.push(loadImage('assets/triangle/00/T0'+i+'.png'));
+      trianglestwo.push(loadImage('assets/triangle/01/T0'+i+'.png'));
+      trianglesthree.push(loadImage('assets/triangle/02/T0'+i+'.png'));
+      trianglesfour.push(loadImage('assets/triangle/03/T0'+i+'.png'));
+      trianglesfive.push(loadImage('assets/triangle/04/T0'+i+'.png'));
+      trianglessix.push(loadImage('assets/triangle/05/T0'+i+'.png'));
+      trianglesseven.push(loadImage('assets/triangle/06/T0'+i+'.png'));
+      triangleseight.push(loadImage('assets/triangle/07/T0'+i+'.png'));
+      trianglesnine.push(loadImage('assets/triangle/08/T0'+i+'.png'));
+      trianglesten.push(loadImage('assets/triangle/09/T0'+i+'.png'));
+    } else if(name === 'C'){
+      circles.push(loadImage('assets/circle/C0'+i+'.png'));
+    } else if(name === 'SC'){
+      semiCircles.push(loadImage('assets/semiCircle/SC0'+i+'.png'));
+    }
+  }
+}
+
 function setup() {
   myCanvas = createCanvas(600 ,600);
   myCanvas.parent("logo");
   //Create Grid
   rectMode(CENTER);
   //rect(windowWidth/2,windowHeight/2,150,150);
-
-  //Create Logo
-  imageMode(CENTER);
-  angleMode(DEGREES);
-  image(circles[0], 300, 300, 600, 600);
-  image(triangles[0], 300, 300, 600, 600);
-  image(semiCircles[0], 300, 300, 600, 600);
-  image(logoName, 300, 300, 600, 600);
 
   //Add Coordinates
   textFont(folio);
@@ -41,6 +63,23 @@ function setup() {
   lastlat = 0;
   lastlon = 0;
   lastalt = 0;
+
+  //Create type selector
+  selector = createSelect();
+  selector.option('Arte negli spazi pubblici');
+  selector.option('Associazioni');
+  selector.option('Collezioni');
+  selector.option('Fondazioni');
+  selector.option('Istituti esteri');
+  selector.option('Musei');
+  selector.option('Musei aziendali e d\'impresa');
+  selector.option('Parchi e Giardini');
+  selector.option('Spazi espositivi');
+  selector.option('Spazi indipendenti');
+  selector.selected('Arte negli spazi pubblici');
+  lastvalue = selector.value();
+  selector.size(100, 20);
+  selector.position(windowWidth/2+250, windowHeight/2+150);
 
   //Create latitude input
   latitude = createInput();
@@ -76,24 +115,18 @@ function setup() {
   saveButton.size(110, 50);
   saveButton.position(windowWidth/2+110, windowHeight/2+250);
   saveButton.mousePressed(randomizeLogo);
-}
 
-function draw() {
-}
-
-function loader(name, num){
-  for(let i = 0; i < num; i++){
-    if(name === 'T'){
-      triangles.push(loadImage('assets/triangle/T0'+i+'.png'));
-    } else if(name === 'C'){
-      circles.push(loadImage('assets/circle/C0'+i+'.png'));
-    } else if(name === 'SC'){
-      semiCircles.push(loadImage('assets/semiCircle/SC0'+i+'.png'));
-    }
-  }
+  //Create Logo
+  imageMode(CENTER);
+  angleMode(DEGREES);
+  image(circles[0], 300, 300, 600, 600);
+  checkTriangle();
+  image(semiCircles[0], 300, 300, 600, 600);
+  image(logoName, 300, 300, 600, 600);
 }
 
 function changeLogo(){
+  lastvalue = selector.value();
   hasinput = true;
   clear();
   if(latitude.value()){
@@ -110,10 +143,9 @@ function changeLogo(){
   r2 = checkFirstNumber(lastlon, r2, true);
   r3 = checkFirstNumber(lastalt, r3, false);
 
-
   //Create Logo
   image(circles[r3], 300, 300, 600, 600);
-  image(triangles[r1], 300, 300, 600, 600);
+  checkTriangle();
   image(semiCircles[r2], 300, 300, 600, 600);
   image(logoName, 300, 300, 600, 600);
 
@@ -145,7 +177,7 @@ function randomizeLogo(){
 
   //Create Logo
   image(circles[r3], 300, 300, 600, 600);
-  image(triangles[r1], 300, 300, 600, 600);
+  checkTriangle();
   image(semiCircles[r2], 300, 300, 600, 600);
   image(logoName, 300, 300, 600, 600);
 
@@ -186,12 +218,7 @@ function windowResized() {
     latitude.remove();
     longitude.remove();
     altitude.remove();
-
-    //Create Logo
-    image(circles[r1], 300, 300, 600, 600);
-    image(triangles[r2], 300, 300, 600, 600);
-    image(semiCircles[r3], 300, 300, 600, 600);
-    image(logoName, 300, 300, 600, 600);
+    selector.remove();
 
     //Add Coordinates
     textFont(folio);
@@ -204,6 +231,22 @@ function windowResized() {
     textAlign(RIGHT);
     text(lastalt, -300+150, 300-160);
     pop();
+
+    //Create type selector
+    selector = createSelect();
+    selector.option('Arte negli spazi pubblici');
+    selector.option('Associazioni');
+    selector.option('Collezioni');
+    selector.option('Fondazioni');
+    selector.option('Istituti esteri');
+    selector.option('Musei');
+    selector.option('Musei aziendali e d\'impresa');
+    selector.option('Parchi e Giardini');
+    selector.option('Spazi espositivi');
+    selector.option('Spazi indipendenti');
+    selector.selected(lastvalue);
+    selector.size(100, 20);
+    selector.position(windowWidth/2+250, windowHeight/2+150);
 
     //Create latitude input
     latitude = createInput();
@@ -239,6 +282,12 @@ function windowResized() {
     saveButton.size(110, 50);
     saveButton.position(windowWidth/2+110, windowHeight/2+250);
     saveButton.mousePressed(randomizeLogo);
+
+    //Create Logo
+    image(circles[r1], 300, 300, 600, 600);
+    checkTriangle();
+    image(semiCircles[r3], 300, 300, 600, 600);
+    image(logoName, 300, 300, 600, 600);
 }
 
 function addInputPlaceholder(hasinput){
@@ -255,5 +304,29 @@ function addInputPlaceholder(hasinput){
       else if(i == 2){input.placeholder = "Altitudine";}
     }
     i++;
+  }
+}
+
+function checkTriangle(){
+  if(selector.value() == 'Arte negli spazi pubblici'){
+    image(trianglesone[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Associazioni'){
+    image(trianglestwo[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Collezioni'){
+    image(trianglesthree[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Fondazioni'){
+    image(trianglesfour[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Istituti esteri'){
+    image(trianglesfive[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Musei'){
+    image(trianglessix[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Musei aziendali e d\'impresa'){
+    image(trianglesseven[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Parchi e Giardini'){
+    image(triangleseight[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Spazi espositivi'){
+    image(trianglesnine[r1], 300, 300, 600, 600);
+  } else if(selector.value() == 'Spazi indipendenti'){
+    image(trianglesten[r1], 300, 300, 600, 600);
   }
 }
